f50b94f61f5b6fa406eff9a3d888f23c8fe4a8f2
Vaishnavi <vaishnavi2073@gmail.com>
2025-10-25 13:56:06 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,35 +11,20 @@ export const Projects = () => {
 
   const projects = [
     {
-      title: "Business Startup",
+      title: "COZMO",
       description: "Design & Development",
       imgUrl: projImg1,
     },
     {
-      title: "Business Startup",
+      title: "Music Player",
       description: "Design & Development",
       imgUrl: projImg2,
     },
     {
-      title: "Business Startup",
+      title: "H&M Reimagined",
       description: "Design & Development",
       imgUrl: projImg3,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
-    },
+    }
   ];
 
   return (
