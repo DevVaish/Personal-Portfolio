90f5d5b15736e00694a6f3ad59edeb6fe52d182b
Vaishnavi <vaishnavi2073@gmail.com>
2025-10-25 13:57:17 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -36,7 +36,7 @@ export const Projects = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <p>Each project I’ve worked on reflects my passion for design, development, and problem-solving. From responsive websites to interactive user interfaces, I focus on creating digital experiences that are both visually appealing and technically strong. Every project has helped me grow as a developer — experimenting with new tools, frameworks, and design systems to build meaningful solutions that bring ideas to life.</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
